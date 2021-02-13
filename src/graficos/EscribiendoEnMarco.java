f7d49a1b8898ba0113c4b2e12d0009b2dc8ee8f5
Alberto <algomu@gmail.com>
2021-02-13 18:43:31 +0100

    Primer texto en marco

---
+++ src/graficos/EscribiendoEnMarco.java
@@ -0,0 +1,39 @@
+package graficos;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class EscribiendoEnMarco {
+    public static void main(String[] args) {
+
+        MarcoConTexto mimarco = new MarcoConTexto();
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
+
+class MarcoConTexto extends JFrame {
+
+    public MarcoConTexto() {
+
+        setVisible(true);
+        setSize(600, 450);
+        setLocation(400, 200);
+        setTitle("Primer texto");
+
+        // No esta escribiendo nada
+        Lamina miLamina=new Lamina();
+        add(miLamina);
+
+    }
+}
+
+class Lamina extends JPanel {
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        g.drawString("TEXTO", 100, 100);
+
+    }
+}
